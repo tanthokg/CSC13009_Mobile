732b0b64acc532c5d8a2efd457c72bae6506cd17
tanthokg <80965785+tanthokg@users.noreply.github.com>
2021-10-21 22:09:34 +0700

    Delete Uyen's HW05/HW05 directory

--- Uyen's HW05/HW05/app/src/main/java/com/example/hw05/FragmentCallbacks.java
+++
@@ -1,5 +0,0 @@
-package com.example.hw05;
-
-public interface FragmentCallbacks {
-    public void onMsgFromMainToFragment(int position);
-}
--- Uyen's HW05/HW05/app/src/main/java/com/example/hw05/MainCallbacks.java
+++
@@ -1,5 +0,0 @@
-package com.example.hw05;
-
-public interface MainCallbacks {
-    public void onMsgFromFragToMain(String sender, int position);
-}
