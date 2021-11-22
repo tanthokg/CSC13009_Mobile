8798840154ab8516371c516076c2c2a59945ed6b
DiemUyen <diemuyen0408@gmail.com>
2021-11-22 19:56:27 +0700

    Update LargeImage.java

--- Final Project/Gallery/app/src/main/java/com/example/gallery/LargeImage.java
+++ Final Project/Gallery/app/src/main/java/com/example/gallery/LargeImage.java
@@ -2,12 +2,15 @@ package com.example.gallery;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.media.MediaScannerConnection;
 import android.os.Bundle;
