d7be1e24d3c7476ed2f2c6b8a294d62597271486
nhatdang2604 <nhatdang2604@gmail.com>
2021-12-21 23:20:12 +0700

    delete project

--- Final Project/Gallery/app/src/main/java/com/example/gallery/FragmentCallbacks.java
+++
@@ -1,7 +0,0 @@
-package com.example.gallery;
-
-import android.graphics.Bitmap;
-
-public interface FragmentCallbacks {
-    public void onMsgFromMainToFrag(Bitmap result);
-}
--- Final Project/Gallery/app/src/main/java/com/example/gallery/MainCallbacks.java
+++
@@ -1,5 +0,0 @@
-package com.example.gallery;
-
-public interface MainCallbacks {
-    public void onMsgFromFragToMain(String sender, String request);
-}
