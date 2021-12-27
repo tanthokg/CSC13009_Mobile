2101df62acbee2ed5b8d90eede4291aee0aeb8b1
Huỳnh Tấn Thọ <80965785+tanthokg@users.noreply.github.com>
2021-12-27 19:49:27 +0700

    Revert "Dang sort"

--- Final Project/Gallery/app/src/main/java/com/example/gallery/ToolbarActionModeCallback.java
+++ Final Project/Gallery/app/src/main/java/com/example/gallery/ToolbarActionModeCallback.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.view.ActionMode;
 
@@ -11,26 +12,22 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class ToolbarActionModeCallback implements ActionMode.Callback{
-    private Context context;
-    private PicturesAdapter picturesAdapter;
-    private ArrayList<File> message_models;
+    private final Context context;
+    private final PicturesAdapter picturesAdapter;
 
-    public ToolbarActionModeCallback(Context context, PicturesAdapter picturesAdapter, ArrayList<File> message_models) {
+    public ToolbarActionModeCallback(Context context, PicturesAdapter picturesAdapter) {
         this.context = context;
         this.picturesAdapter = picturesAdapter;
-        this.message_models = message_models;
     }
 
     @Override
     public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-        mode.getMenuInflater().inflate(R.menu.toolbar_multiple_items, menu);//Inflate the menu over action mode
+        mode.getMenuInflater().inflate(R.menu.toolbar_multiple_items, menu);
         return true;
     }
     @Override
     public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-        menu.findItem(R.id.delete).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
-        menu.findItem(R.id.share).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
-
+        menu.findItem(R.id.recoverMulti).setVisible(false);
         return true;
     }
 
@@ -38,20 +35,24 @@ public class ToolbarActionModeCallback implements ActionMode.Callback{
     public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
         if(item.getItemId() == R.id.delete)
         {
-            //Delete Multiple Images
+            // Delete Multiple Images
             ((MainActivity)context).picturesFragment.deleteMulti();
-            mode.finish();
-            ((MainActivity)context).bottomNavigationView.setVisibility(View.VISIBLE);
         }
         else if (item.getItemId() == R.id.share)
         {
-            //Share Multiple Images
+            // Share Multiple Images
             ((MainActivity)context).picturesFragment.shareMulti();
-            mode.finish();
-            ((MainActivity)context).bottomNavigationView.setVisibility(View.VISIBLE);
         }
-
-        return false;
+        else if (item.getItemId() == R.id.selectAll)
+        {
+            Toast.makeText(context.getApplicationContext(), "Select all", Toast.LENGTH_SHORT).show();
+            ((MainActivity)context).picturesFragment.selectAll();
+        }
+        else if (item.getItemId() == R.id.addToAlbum)
+        {
+            ((MainActivity)context).picturesFragment.addToAlbum();
+        }
+        return true;
     }
 
     @Override
